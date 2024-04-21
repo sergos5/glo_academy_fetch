3861000021800b8f05032008b5306c7a5a837481
Sergey <sergos5@yandex.ru>
2024-04-21 11:05:04 +0300

    отправка через XMLHttpRequest

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const getData = (url) => {
     fetch(url) 
-    .then(response => {
-        console.log('Данные получены');
-        return response.json();
-    })
-    .then(data => {        
-        sendData(JSON.stringify(data));
-        console.log(data);
-    })
+    .then(response => response.json())
+    .then(data => sendData(JSON.stringify(data)))
     .catch(() => alert('Error'));
 };
 
 
 const sendData = (data) => {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
+    const request = new XMLHttpRequest();
+    request.open('POST', 'https://jsonplaceholder.typicode.com/posts');
+    request.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+    request.send(data);
+
+
+    /* fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
         body: data,
         headers: {
             'Content-type': 'application/json; charset=UTF-8'
         }
     })
-    .then(response => console.log(response));
+    .then(response => console.log(response)); */
 };
 
 getData('db.json');
